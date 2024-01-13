6bd475d2c87a44a86c97c6beff725bae81f0c50c
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2024-01-13 19:15:30 +0000

    allowed methods

--- src/app/api/convert-image/route.ts
+++ src/app/api/convert-image/route.ts
@@ -113,6 +113,11 @@ export const POST = async (req: Request) => {
       },
       {
         status: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
       }
     );
   } catch (error) {
